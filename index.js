48ee9885a668302de6d5efdc898980ddb8a01433
deepak640 <ayushdeepnegi@gmail.com>
2024-07-14 23:45:43 +0530

    updated with image prompt

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 var app = express();
+const fs = require('fs')
 require("dotenv").config();
 
 const { GoogleGenerativeAI } = require("@google/generative-ai");
@@ -7,7 +8,7 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 // Access your API key as an environment variable (see "Set up your API key" above)
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 
-async function run() {
+async function textPrompt() {
   // The Gemini 1.5 models are versatile and work with both text-only and multimodal prompts
   const model = genAI.getGenerativeModel({
     model: "gemini-1.5-flash",
@@ -21,15 +22,31 @@ how to make mango juice
   const result = await model.generateContent(prompt);
   const response = await result.response;
   const text = response.text();
-  console.log("🚀 ~ run ~ text:", text)
+  console.log("🚀 ~ run ~ text:", text);
   return text;
 }
+
+const imagePrompt = async () => {
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash-latest" });
+
+  const prompt = "what is this and describe this";
+  const image = {
+    inlineData: {
+      data: Buffer.from(fs.readFileSync("food.jpg")).toString("base64"),
+      mimeType: "image/jpg",
+    },
+  };
+
+  const result = await model.generateContent([prompt, image]);
+  console.log(result.response.text());
+};
 
 app.listen(3000, () => {
   console.log("Listening ...");
 });
 
 app.get("/", async (req, res) => {
-  const paragraph = await run();
-  res.send(paragraph)
+  // const paragraph = await textPrompt();
+  const paragraph = await imagePrompt();
+  res.send(paragraph);
 });
